57991484bad4985af546071108a9a1bc2e81bb2f
Filomaticom <192216517+FiloFromZero@users.noreply.github.com>
2025-12-01 11:30:54 -0500

    Configure S3 backend and fix pipeline order

--- Back-Kata/terraform/provider.tf
+++ Back-Kata/terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "back-kata-terraform-state"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
