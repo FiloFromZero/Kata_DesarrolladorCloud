56e7efa4df265b17712ad1c32b7b56729598552e
Filomaticom <192216517+FiloFromZero@users.noreply.github.com>
2025-12-01 14:53:12 -0500

    Espero que sea el último Commit

--- Front-Kata/src/app/app.config.ts
+++ Front-Kata/src/app/app.config.ts
@@ -11,6 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptors([apiUrlInterceptor, authInterceptor]), withFetch())
+    provideHttpClient(withInterceptors([authInterceptor, apiUrlInterceptor]), withFetch())
   ]
 };
