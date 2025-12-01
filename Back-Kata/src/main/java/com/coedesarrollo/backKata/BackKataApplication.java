1d4994e0f42124b911ecf76f4b05bc6ea0dd603f
Filomaticom <192216517+FiloFromZero@users.noreply.github.com>
2025-12-01 13:59:35 -0500

    fix: exclude MailHealthContributorAutoConfiguration to resolve 503 error

--- Back-Kata/src/main/java/com/coedesarrollo/backKata/BackKataApplication.java
+++ Back-Kata/src/main/java/com/coedesarrollo/backKata/BackKataApplication.java
@@ -3,7 +3,9 @@ package com.coedesarrollo.backKata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.actuate.autoconfigure.mail.MailHealthContributorAutoConfiguration;
+
+@SpringBootApplication(exclude = {MailHealthContributorAutoConfiguration.class})
 public class BackKataApplication {
 
     public static void main(String[] args) {
